3be256ebd9df4fd28a754af30a55e58c93ccdbd1
Nguyễn Phúc Vinh <nguyenphucvinh05072001@gmail.com>
2022-04-01 18:41:57 +0700

    add ID columns to table account

--- usersDDL.sql
+++ usersDDL.sql
@@ -8,14 +8,15 @@ CREATE TABLE IF NOT EXISTS department (
 );
 
 CREATE TABLE IF NOT EXISTS account (
-    username VARCHAR(20) PRIMARY KEY,
-    `name` VARCHAR(30) NOT NULL,
+    ID VARCHAR(8) PRIMARY KEY,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    `name` VARCHAR(30),
     phone VARCHAR(15),
     email VARCHAR(50) ,
     `password` VARCHAR(100) NOT NULL,
     avatarURL VARCHAR(255),
     birthday DATE,
-    dname VARCHAR(20),
+    dname VARCHAR(20) NOT NULL,
     FOREIGN KEY (dname) REFERENCES department(dname)
         ON DELETE SET NULL ON UPDATE CASCADE
 );
