6e39eedad6544ebd274378138cd08b5510b76b6e
Nguyễn Phúc Vinh <nguyenphucvinh05072001@gmail.com>
2022-04-17 16:08:45 +0700

    change from Node to Java

--- TFService/routes/index.js
+++
@@ -1,7 +0,0 @@
-const transactionRouter = require('./transaction.route')
-
-function routes(app) {
-    app.use('/api/transactions', transactionRouter)
-}
-
-module.exports = routes
---
+++ TFService/src/main/java/ticklab/tfservice/TfserviceApplication.java
@@ -0,0 +1,13 @@
+package ticklab.tfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfserviceApplication.class, args);
+	}
+
+}
