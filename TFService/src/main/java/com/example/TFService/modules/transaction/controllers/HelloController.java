2c1c0b77e2b36a76af27d08256ce6c6a45ea2619
phucvinh57 <npvinh0507@gmail.com>
2022-06-19 16:27:37 +0700

    add folder structrue

--- TFService/src/main/java/com/example/TFService/modules/transaction/controllers/HelloController.java
+++ TFService/src/main/java/com/example/TFService/modules/transaction/controllers/HelloController.java
@@ -1,14 +1,12 @@
-package com.example.TFService;
+package com.example.TFService.modules.transaction.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
-
 }
