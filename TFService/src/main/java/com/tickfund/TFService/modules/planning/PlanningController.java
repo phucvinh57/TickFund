fe5b19cfbf309fc6f8dc939d454ac240f578611b
phucvinh57 <npvinh0507@gmail.com>
2022-07-02 11:25:51 +0700

    fix bug database do not auto create

--- TFService/src/main/java/com/tickfund/TFService/modules/planning/PlanningController.java
+++ TFService/src/main/java/com/tickfund/TFService/modules/planning/PlanningController.java
@@ -1,6 +1,6 @@
 package com.tickfund.TFService.modules.planning;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tickfund.TFService.commons.enums.CycleEnum;
 import com.tickfund.TFService.commons.enums.RepetitionModeEnum;
 import com.tickfund.TFService.modules.planning.entity.Planning;
-import com.tickfund.TFService.modules.planning.repository.UserRepository;
+// import com.tickfund.TFService.modules.planning.repository.UserRepository;
 // import com.tickfund.TFService.modules.planning.repository.PlanningRepository;
 import com.tickfund.TFService.modules.planning.vo.PlanningCycleVO;
 import com.tickfund.TFService.modules.planning.vo.PlanningRepetitionVO;
@@ -27,8 +27,8 @@ import java.util.ArrayList;
 public class PlanningController {
 	// @Autowired
 	// private PlanningRepository repo;
-	@Autowired
-	private UserRepository repo;
+	// @Autowired
+	// private UserRepository repo;
 
 	@GetMapping("")
 	@ResponseBody
