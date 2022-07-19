b524631372a2a5872799bb04fc81c6d5d71132dc
phucvinh57 <npvinh0507@gmail.com>
2022-07-19 20:36:33 +0700

    ignore .env file

--- db/init/users_example.sql
+++ db/init/users_example.sql
@@ -1,11 +1,11 @@
-CREATE USER IF NOT EXISTS 'auth_service';
-GRANT SELECT ON `ticklab_users`.department TO 'auth_service';
-GRANT SELECT, INSERT ON `ticklab_users`.account TO 'auth_service';
+-- CREATE USER IF NOT EXISTS 'auth_service';
+-- GRANT SELECT ON `ticklab_users`.department TO 'auth_service';
+-- GRANT SELECT, INSERT ON `ticklab_users`.account TO 'auth_service';
 
--- Replace password
-ALTER USER 'auth_service' IDENTIFIED WITH mysql_native_password BY '<password>';
+-- -- Replace password
+-- ALTER USER 'auth_service' IDENTIFIED WITH mysql_native_password BY '<password>';
 
-CREATE USER IF NOT EXISTS `tickfund_app`;
-GRANT ALL ON tickfund.* TO 'tickfund_app';
-ALTER USER 'tickfund_app' IDENTIFIED WITH mysql_native_password BY '<password>';
+-- CREATE USER IF NOT EXISTS `tickfund_app`;
+-- GRANT ALL ON tickfund.* TO 'tickfund_app';
+-- ALTER USER 'tickfund_app' IDENTIFIED WITH mysql_native_password BY '<password>';
 
