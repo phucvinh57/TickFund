1ec36a243762ab50edf3c51f79265f466eab76b5
phucvinh57 <npvinh0507@gmail.com>
2022-08-10 16:17:07 +0700

    edit DDL in db/init

--- db/init/3-createDbUsers.sql
+++ db/init/3-createDbUsers.sql
@@ -1,9 +1,11 @@
-CREATE USER IF NOT EXISTS 'auth_service';
-GRANT SELECT ON `ticklab_users`.department TO 'auth_service';
-GRANT SELECT, INSERT ON `ticklab_users`.account TO 'auth_service';
+DROP USER 'auth_service'@'%';
+DROP USER 'tickfund_app'@'%';
 
-ALTER USER 'auth_service' IDENTIFIED WITH mysql_native_password BY '123456';
+CREATE USER 'auth_service'@'%';
+GRANT SELECT ON `ticklab_users`.department TO 'auth_service'@'%';
+GRANT SELECT, INSERT ON `ticklab_users`.account TO 'auth_service'@'%';
+ALTER USER 'auth_service'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
 
-CREATE USER IF NOT EXISTS `tickfund_app`;
-GRANT ALL ON tickfund.* TO 'tickfund_app';
-ALTER USER 'tickfund_app' IDENTIFIED WITH mysql_native_password BY '123456';
+CREATE USER `tickfund_app`@'%';
+GRANT ALL ON tickfund.* TO 'tickfund_app'@'%';
+ALTER USER 'tickfund_app'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
