0b8f52e1ce7a92cda39764b419d2f17548ae09af
phucvinh57 <npvinh0507@gmail.com>
2022-08-17 09:24:28 +0700

    merge code feature/user

--- databases/createDbUsers_example.sql
+++ databases/createDbUsers_example.sql
@@ -4,8 +4,12 @@ DROP USER IF EXISTS 'tickfund_app'@'%';
 CREATE USER 'auth_service'@'%';
 GRANT SELECT ON `ticklab_users`.* TO 'auth_service'@'%';
 GRANT INSERT, UPDATE ON `ticklab_users`.account TO 'auth_service'@'%';
-ALTER USER 'auth_service'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
+
+-- Fill the password below
+ALTER USER 'auth_service'@'%' IDENTIFIED WITH mysql_native_password BY '';
 
 CREATE USER `tickfund_app`@'%';
 GRANT ALL ON tickfund.* TO 'tickfund_app'@'%';
-ALTER USER 'tickfund_app'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
+
+-- Fill the password below
+ALTER USER 'tickfund_app'@'%' IDENTIFIED WITH mysql_native_password BY '';
