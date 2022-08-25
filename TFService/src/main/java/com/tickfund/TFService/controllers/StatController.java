b3257dcdfc90fea581044c5a26d716c74061a6c6
phucvinh57 <npvinh0507@gmail.com>
2022-08-25 22:36:55 +0700

    add nocache for auth service

--- TFService/src/main/java/com/tickfund/TFService/controllers/StatController.java
+++ TFService/src/main/java/com/tickfund/TFService/controllers/StatController.java
@@ -3,12 +3,9 @@ package com.tickfund.TFService.controllers;
 import com.tickfund.TFService.commons.enums.CycleEnum;
 import com.tickfund.TFService.dtos.in.StatDTO;
 import com.tickfund.TFService.dtos.out.StatOut;
-import com.tickfund.TFService.entities.StatBucket;
 import com.tickfund.TFService.services.StatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/stat")
