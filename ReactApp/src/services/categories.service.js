da7f83dd9454ac72206fa378e4ab0c4e06a38636
TuanKietTran <kyletran101.work@gmail.com>
2022-08-28 13:33:05 +0700

    Add fab

---
+++ ReactApp/src/services/categories.service.js
@@ -0,0 +1,32 @@
+import http from './httpCommon'
+
+const path = '/categories'
+
+const getAll = async () => {
+    let response = await http.get(path)
+    return response
+}
+
+const addNew = async data => {
+    let response = await http.post(path, data)
+    return response
+}
+
+const update = async (name, data) => {
+    let response = await http.put(`${path}/${name}`)
+    return response
+}
+
+const remove = async (name) => {
+    let response = await http.delete(`${path}/${name}`)
+    return response
+}
+
+const categoriesService = {
+    getAll,
+    addNew,
+    update,
+    remove
+}
+
+export default categoriesService
