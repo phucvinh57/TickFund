9286b7c33dfc9793dd75f9854ef2b44f7fad1f24
phucvinh57 <npvinh0507@gmail.com>
2022-08-30 13:11:22 +0700

    add deploy script for AuthService

--- AuthService/index.ts
+++ AuthService/index.ts
@@ -15,7 +15,8 @@ app.use(nocache())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(express.static(__dirname + '/node_modules/bootstrap/dist'))
+app.use("/static", express.static(__dirname + '/node_modules/bootstrap/dist'))
+app.use("/static", express.static(__dirname + '/public/images'))
 
 route(app)
 
